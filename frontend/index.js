206b7c3cf3922073f658b6536ea6a9f18b953cbb
Abdullah Barudgar <69586404+AbdullahBarudgar@users.noreply.github.com>
2024-08-13 18:15:36 +0530

    Project dependencies

---
+++ frontend/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <h1>Abdullah, Barudgar!</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
