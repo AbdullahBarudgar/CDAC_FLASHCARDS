c0aa1a4028858d64dfad0367f70217b07315360c
Abdullah Barudgar <69586404+AbdullahBarudgar@users.noreply.github.com>
2024-08-15 20:15:27 +0530

    intial

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Abdullah Barudgar Server');
 });
 
 app.listen(3000, () => {
