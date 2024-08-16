63d5314207111e3fa4a50f83ca4ca5bfe104abbd
Abdullah Barudgar <69586404+AbdullahBarudgar@users.noreply.github.com>
2024-08-16 18:39:19 +0530

    Delete frontend directory

--- frontend/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => {
-  return <h1>Abdullah, Barudgar!</h1>;
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
