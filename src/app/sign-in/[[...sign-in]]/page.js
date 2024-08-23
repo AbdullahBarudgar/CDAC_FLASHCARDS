1a096ff3c1270a004c66b9b9d89d1f976451f0a7
Abdullah Barudgar <abdullahbarudgar@hotmail.com>
2024-08-23 21:45:19 +0530

    Delete src directory

--- src/app/sign-in/[[...sign-in]]/page.js
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <SignIn />
-    </div>
-  );
-}
